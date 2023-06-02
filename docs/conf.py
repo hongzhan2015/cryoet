bfcdbb1390edf4796a5fd66968db752b9263421d
Hong Zhan <bluetutu2009@gmail.com>
2023-06-02 08:59:06 -0500

    modify files

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,9 @@ author = 'Hong Zhan'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'versionwarning.extension',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +25,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+html_theme_options = {
+    'sticky_navigation': True,
+    'logo_only' : 'True',
+    'style_nav_header_background' : '#5a8089',
+}
+
+extensions += ['sphinx-prompt']
+
+def setup(app):
+    app.add_js_file("version-alert.js")
