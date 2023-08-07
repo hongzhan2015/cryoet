4944f6ba29f76802d9c7cb27832a52dc686da323
Hong Zhan <bluetutu2009@gmail.com>
2023-08-07 11:37:07 -0500

    add new scripts that taking command line arguments

---
+++ scripts/order_generate_use_name.py
@@ -0,0 +1,60 @@
+#!/usr/env/python
+
+import os, re, sys
+
+options = sys.argv
+#print(options)
+
+file_name = options[1]
+
+def createList(numbers):
+    return list(range(numbers+1))
+
+def reorder(file_name, group = int(3), increment = int(3), max_pos_angle = int(54), min_neg_angle = int(-54), scheme = "pos"):
+    #path = os.getcwd()
+    #for filename in os.listdir(path):
+        if file_name.endswith(".tlt"):
+           tiltname = file_name.split(".")[0]
+           extension = ".order"
+           ordername = tiltname + extension
+           tiltfile = open(file_name, 'r').read().splitlines()
+           num_angles = int(abs(max_pos_angle - min_neg_angle)/increment)
+           num_group = int(num_angles/group)
+           reorderlist = createList(num_angles)
+            ## create an empty angle dictionary ##
+           angle_dic = dict.fromkeys(reorderlist)
+           zero_tilt = tiltfile[int(num_angles/2)]
+           angle_dic[0] = zero_tilt
+
+            ## assign value to key ##
+           for cluster in range(num_group):
+               if (cluster % 2) == 0:
+                   for i in range(1,increment+1):
+                       factor_1 = int((cluster/2)*(group+increment))
+                       factor_2 = int((cluster/2)*group)
+                       index = int(int(num_angles/2) + factor_2 + i)
+                       angle_dic[factor_1 + i] = tiltfile[index]
+               else:
+                   for j in range(1,increment+1):
+                       factor_1 = int(((cluster -1)/2)*(group+increment))
+                       factor_2 = int(((cluster-1)/2)*group)
+                       index = int(int(num_angles/2) - factor_2 -j)
+                       angle_dic[factor_1 + group + j] = tiltfile[index]
+
+           reorderline = []
+           for key, value in angle_dic.items():
+               reorderline.append(value)
+
+           reorderfile = open(ordername, 'w') 
+           for lines in reorderline:
+               reorderfile.writelines(lines + "\n")
+           reorderfile.close()
+        else:
+            print("Please provide tile file with extension .tlt")
+			
+			
+if __name__ == '__main__':
+     
+    # Calling main() function
+    #print(file_name)
+    reorder()
